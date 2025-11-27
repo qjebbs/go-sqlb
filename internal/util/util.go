2b50d740606d54692c88ae4c73862486bd7f63ae
jebbs <qjebbs@gmail.com>
2025-11-27 15:17:48 +0800

    first commit... * This commit is extracted from github.com/qjebbs/go-sqlf/v3

---
+++ internal/util/util.go
@@ -0,0 +1,14 @@
+package util
+
+// Concat concatenates multiple slices into one slice.
+func Concat[T any](slices ...[]T) []T {
+	totalLen := 0
+	for _, s := range slices {
+		totalLen += len(s)
+	}
+	result := make([]T, 0, totalLen)
+	for _, s := range slices {
+		result = append(result, s...)
+	}
+	return result
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Go SQL QueryBuilder
+
+Package sqlb provides a complex SQL query builder shipped  with WITH-CTE / JOIN 
+Elimination capabilities, while [go-sqlf](https://github.com/qjebbs/go-sqlf) is the underlying foundation.
+
+See [example_test.go](./example_test.go) for examples.

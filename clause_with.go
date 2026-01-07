408359b1cc4dd3995b9f32ea5903b3473d569c0b
jebbs <qjebbs@gmail.com>
2026-01-07 15:06:37 +0800

    Remove unused codes which make clauseWith a Builder

--- clause_with.go
+++ clause_with.go
@@ -1,20 +1,14 @@
 package sqlb
 
 import (
-	"context"
 	"fmt"
 	"strings"
 
 	"github.com/qjebbs/go-sqlf/v4"
 )
 
-var _ sqlf.Builder = (*clauseWith)(nil)
-var _ Builder = (*clauseWith)(nil)
-
 // clauseWith represents a SQL WITH clause.
 type clauseWith struct {
-	builder sqlf.Builder
-
 	debugger
 	ctes     []*cte
 	ctesDict map[string]*cte // the actual cte names, not aliases
@@ -49,76 +43,8 @@ func (w *clauseWith) With(table Table, builder sqlf.Builder) *clauseWith {
 	return w
 }
 
-// For sets the main query builder the CTEs is for.
-func (w *clauseWith) For(builder sqlf.Builder) *clauseWith {
-	w.resetDepTablesCache()
-	w.builder = builder
-	return w
-}
-
-// BuildQuery implements Builder
-func (w *clauseWith) BuildQuery(style sqlf.BindStyle) (string, []any, error) {
-	return w.BuildQueryContext(context.Background(), style)
-}
-
-// BuildQueryContext builds the query with the given context.
-func (w *clauseWith) BuildQueryContext(ctx context.Context, style sqlf.BindStyle) (query string, args []any, err error) {
-	if w == nil {
-		return "", nil, nil
-	}
-	buildCtx := sqlf.NewContext(ctx, style)
-	query, err = w.Build(buildCtx)
-	if err != nil {
-		return "", nil, err
-	}
-	args = buildCtx.Args()
-	return query, args, nil
-}
-
-// Build implements sqlf.Builder
-func (w *clauseWith) Build(ctx *sqlf.Context) (string, error) {
-	if w == nil {
-		return "", nil
-	}
-	ctx, pruning := decideContextPruning(ctx, w.pruning)
-	if !pruning {
-		return w.BuildRequired(ctx, nil)
-	}
-	required, unresolved, err := w.collectDependencies(ctx)
-	if err != nil {
-		return "", err
-	}
-
-	if deps := dependenciesFromContext(ctx); deps != nil {
-		for table := range unresolved.SourceNames {
-			// report undefined dependencies to parent query builder
-			// if b.debug && b.debugName != "" {
-			// 	fmt.Printf("[%s] reporting dependency table: %s\n", b.debugName, name)
-			// }
-			deps.SourceNames[table] = true
-		}
-		for table := range unresolved.OuterTables {
-			// report undefined dependencies to parent query builder
-			// if b.debug && b.debugName != "" {
-			// 	fmt.Printf("[%s] reporting dependency table: %s\n", b.debugName, name)
-			// }
-			deps.OuterTables[table] = true
-		}
-		// collecting dependencies only,
-		// no need to build anything here
-		return "", nil
-	}
-	return w.BuildRequired(ctx, required)
-}
-
 // BuildRequired builds the WITH clause including only the required CTEs.
 func (w *clauseWith) BuildRequired(ctx *sqlf.Context, required map[string]bool) (query string, err error) {
-	if w.builder != nil {
-		query, err = w.builder.Build(ctx)
-	}
-	if len(w.ctes) == 0 {
-		return query, nil
-	}
 	pruning := pruningFromContext(ctx)
 	cteClauses := make([]string, 0, len(w.ctes))
 	for _, cte := range w.ctes {
@@ -137,38 +63,10 @@ func (w *clauseWith) BuildRequired(ctx *sqlf.Context, required map[string]bool) 
 		}
 	}
 	if len(cteClauses) == 0 {
-		return query, nil
+		return "", nil
 	}
 	withClauses := "WITH " + strings.Join(cteClauses, ", ")
-	if query == "" {
-		return withClauses, nil
-	}
-	return withClauses + " " + query, nil
-}
-
-func (w *clauseWith) collectDependencies(ctx *sqlf.Context) (required map[string]bool, unresolved *dependencies, err error) {
-	if w.builder == nil {
-		return nil, nil, nil
-	}
-	if w.deps != nil {
-		return w.deps, w.unresolvedDeps, nil
-	}
-	deps := newDependencies(w.name)
-	// use a separate context to avoid polluting args
-	ctx = sqlf.NewContext(ctx, sqlf.BindStyleQuestion)
-	ctx = contextWithDependencies(ctx, deps)
-	// collect dependencies from query builder
-	_, err = w.builder.Build(ctx)
-	if err != nil {
-		return nil, nil, err
-	}
-	required, unresolved, err = w.CollectDependenciesForDeps(ctx, deps)
-	if err != nil {
-		return nil, nil, err
-	}
-	w.deps = required
-	w.unresolvedDeps = unresolved
-	return required, unresolved, nil
+	return withClauses, nil
 }
 
 // CollectDependenciesForDeps collects the table dependencies for specific deps
